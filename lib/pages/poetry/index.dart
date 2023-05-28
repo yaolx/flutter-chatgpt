3635b6bb6843bf21fa1cbf96588373455c26565c
yaolx <64489456@qq.com>
2023-05-28 09:35:25 +0800

    fix：调整翻页效果

--- lib/pages/poetry/index.dart
+++ lib/pages/poetry/index.dart
@@ -1,7 +1,6 @@
 import 'package:flutter_chatgpt/model/poetry.dart';
 import 'package:flutter_chatgpt/api/poetry.dart';
 import 'package:flutter/material.dart';
-import 'package:bookfx/bookfx.dart';
 
 class Poetry extends StatefulWidget {
   const Poetry({super.key});
@@ -13,8 +12,6 @@ class Poetry extends StatefulWidget {
 class _ChatPageState extends State<Poetry> {
   late List _poetryList = <PoetryModel>[];
   late int _curPage = 1;
-  EBookController eBookController = EBookController();
-  BookController bookController = BookController();
 
   @override
   void initState() {
@@ -39,46 +36,32 @@ class _ChatPageState extends State<Poetry> {
             ? null
             : Stack(
                 children: [
-                  BookFx(
-                    size: Size(MediaQuery.of(context).size.width,
-                        MediaQuery.of(context).size.height),
-                    pageCount: _poetryList.length,
-                    currentPage: (index) {
-                      return Center(
-                        child: Column(
-                          mainAxisAlignment: MainAxisAlignment.center,
-                          children: [
-                            Text(
-                              _poetryList[index]['title'],
-                            ),
-                            Text(
-                              _poetryList[index]['body'],
-                            ),
-                          ],
+                  PageView(
+                    controller: PageController(
+                      viewportFraction: 1,
+                    ),
+                    onPageChanged: (int index) {
+                      setState(() {
+                        _curPage = index + 1;
+                      });
+                    },
+                    children: <Widget>[
+                      ..._poetryList.map(
+                        (poetry) => Center(
+                          child: Column(
+                            mainAxisAlignment: MainAxisAlignment.center,
+                            children: [
+                              Text(
+                                poetry['title'],
+                              ),
+                              Text(
+                                poetry['body'],
+                              ),
+                            ],
+                          ),
                         ),
-                      );
-                    },
-                    lastCallBack: (index) {
-                      setState(() {
-                        if (index != 0 && index != _curPage) {
-                          _curPage = index;
-                        }
-                      });
-                    },
-                    nextCallBack: (index) {
-                      setState(() {
-                        if (index != _curPage) {
-                          _curPage = index;
-                        }
-                      });
-                    },
-                    nextPage: (index) {
-                      return Row(
-                        mainAxisAlignment: MainAxisAlignment.center,
-                        children: [Text('下一首：${_poetryList[index]['title']}')],
-                      );
-                    },
-                    controller: bookController,
+                      ),
+                    ],
                   ),
                   Positioned(
                     right: 10,
